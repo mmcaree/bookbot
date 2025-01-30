bb3e86634178b2b8960ff923c61cbfb05fbca949
mmcaree <mmcaree007@gmail.com>
2025-01-29 21:32:55 -0500

    define main func to open txt file and print to console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+
+def main():
+    path_to_file = "/home/memcaree/dev/projects/bookbot/books/frankenstein.txt"
+
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+main()
+
